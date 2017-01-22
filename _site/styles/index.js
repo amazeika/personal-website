3ad7591a87f569e91cbee7c78fd8f7fd468e88fa
Arunas Mazeika <arunas@timble.net>
2017-01-22 23:00:22 +0100

    Compile

--- _site/styles/index.js
+++ _site/styles/index.js
@@ -1,20 +1,47 @@
-var hide_lists = function(cb) {
-    $('#posts').fadeOut(300);
-    $('#projects').fadeOut(300);
-    $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled')
-};
-var show_projects = function() {
-    $('#posts-btn').removeClass('disabled');
-    $('#posts').fadeOut(300, function() {
-        $('#projects').fadeIn(300)
+var lists = ['posts', 'projects', 'experiences', 'education', 'skills'];
+
+var hide_lists = function(cb)
+{
+    $.each(lists, function (idx, label) {
+        var selector = '#' + label;
+        $(selector).fadeOut(300);
+        selector += '-btn';
+        $(selector).removeClass('disabled');
     });
-    $('#projects-btn').addClass('disabled')
 };
-var show_posts = function() {
-    $('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(function() {
-        $('#posts').fadeIn(300)
+
+var show_list = function(label)
+{
+    var selector = '#' + label;
+    var current = $(selector);
+    selector += '-btn';
+    var current_btn = $(selector);
+
+    var active;
+
+    $.each(lists, function(idx, label)
+    {
+        var selector = '#' + label + '-btn';
+        var el = $(selector);
+
+        if (el.hasClass('disabled')) {
+            active = label;
+            return false;
+        }
     });
-    $('#posts-btn').addClass('disabled')
+
+    if (active != label)
+    {
+        if (active)
+        {
+            $('#' + active + '-btn').removeClass('disabled');
+
+            $('#' + active).fadeOut(300, function() {
+                $(current).fadeIn(300);
+            });
+        }
+        else $(current).fadeIn(300);
+
+        current_btn.addClass('disabled');
+    }
 };
